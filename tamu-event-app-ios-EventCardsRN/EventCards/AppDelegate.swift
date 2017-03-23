e23bd300be470b3445d3098d3524979b47e9cdfd
insertnamehere77 <Kaleeb@Kalebs-MacBook-Pro.local>
2017-03-23 10:51:07 -0500

    Firebase Added
    Added Firebase functionality to last night’s new master branch. Use the
    xcworkspace to open the project from now on.

--- tamu-event-app-ios-EventCardsRN/EventCards/AppDelegate.swift
+++ tamu-event-app-ios-EventCardsRN/EventCards/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 
 
@@ -31,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
     
